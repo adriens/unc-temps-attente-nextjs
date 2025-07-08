eb1a123028cd83dcfdacea3eedc32dfd24f96431
SpewQ <speewq@gmail.com>
2025-07-08 17:10:17 +1100

    0.1

--- find_agency_v0/pages/index.js
+++ find_agency_v0/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
@@ -24,6 +25,10 @@ export default function Home({ allPostsData }) {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        <h1 className={utilStyles.headingLg}>
+          <Link href="/posts/agences">Test agences</Link>
+        </h1>
+
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
